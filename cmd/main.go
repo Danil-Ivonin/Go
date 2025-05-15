7ead9e07854dec6ee5835605684cbea2159ef10a
D.Ivonin <D.Ivonin@tanto-s.ru>
2025-05-15 23:49:55 +0300

    Base proj structure

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
-	todo "github.com/Danil-Ivonin/Go"
+	"github.com/Danil-Ivonin/Go"
 	"github.com/Danil-Ivonin/Go/pkg/handler"
+	"github.com/Danil-Ivonin/Go/pkg/repository"
+	"github.com/Danil-Ivonin/Go/pkg/service"
 	"log"
 )
 
 func main() {
 	srv := new(todo.Server)
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error occired while running server: %v", err)
 	}
